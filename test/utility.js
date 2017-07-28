8ffa05345717ce053114848c27b99a9aec5d401b
Joel Dentici <joeldentici@cox.net>
2017-07-28 15:37:43 -0700

    Added test stuff

---
+++ test/utility.js
@@ -0,0 +1,14 @@
+const λ = require('fantasy-check/src/adapters/nodeunit');
+const applicative = require('fantasy-check/src/laws/applicative');
+const functor = require('fantasy-check/src/laws/functor');
+const monad = require('fantasy-check/src/laws/monad');
+
+const {identity, constant} = require('fantasy-combinators');
+
+const {equals} = require('../test-lib.js');
+
+const Sugar = require('../src/sugar.js');
+
+exports.Utility = {
+
+};
